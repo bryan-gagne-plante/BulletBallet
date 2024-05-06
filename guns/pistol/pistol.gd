25071f5a754af5a37d251d3f21716edcaa6c8760
Sam-Existence <samfiblackwell@gmail.com>
2024-05-06 14:25:58 -0400

    Ajout de fonctionnalité de tirer automatiquement

--- guns/pistol/pistol.gd
+++ guns/pistol/pistol.gd
@@ -5,3 +5,15 @@ func _physics_process(delta):
 	if enemies_in_range.size() > 0:
 		var target_enemy = enemies_in_range.front()
 		look_at(target_enemy.global_position)
+
+func shoot():
+	const BULLET = preload("res://guns/pistol/bullet.tscn")
+	var new_bullet = BULLET.instantiate()
+	new_bullet.global_position = %Barrel.global_position
+	new_bullet.global_rotation = %Barrel.global_rotation
+	%Barrel.add_child(new_bullet)
+	
+
+
+func _on_timer_timeout():
+	shoot()
